9b5e94b148f017fbdcbf00bb2caf319a4a8aa64a
vinicius-hayden <vinihaydenss@gmail.com>
2022-07-05 11:41:11 -0300

    feature, ngb and httpclient modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    FeatureModule
   ],
   providers: [],
   bootstrap: [AppComponent]
